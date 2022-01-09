15f58d8499381715a8c02cae81a4f3cc7c8084da
vchkoz <vchkoz@mail.ru>
2022-01-09 21:07:38 +0300

    test

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,13 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello World!'
+
+
+@app.route('/new', method=['POST'])
+def neww():
+    content = request.get_json()
+    print(content)
+    return str(content)
 
 
 if __name__ == '__main__':
