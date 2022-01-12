77fa96c6bbbd98d1399b50b18c954ca1e45f6007
vchkoz <vchkoz@mail.ru>
2022-01-12 17:32:51 +0300

    Сделан приём csv файла на динамический URL

--- app.py
+++ app.py
@@ -10,15 +10,16 @@ def hello_world():
     return 'Hello, this is the WHITE BREAD page'
 
 
-@app.route('/serial_number_will_be_here', methods=['POST'])
-def upload():
+@app.route('/upload/<serial_number>', methods=['POST'])
+def profile(serial_number):
     file = io.StringIO(request.files['file'].stream.read().decode("UTF-8"), newline=None)
     reader = csv.reader(file, delimiter=';')
     for line in reader:
         print(line) # тут будут выводиться принятые данные в виде списка
+    print(serial_number) # тут будет выводиться серийный номер АКЭС
 
     #     отправить файл.csv можно через коммандную сторку:
-    #     curl -F "file=@example.csv" http://localhost:5000/serial_number_will_be_here
+    #     curl -F "file=@example.csv" http://localhost:5000/upload/<Здесь будет серийный номер>
 
     return 'success'
 
