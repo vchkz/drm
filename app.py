e3d87ef981482c915d49215f93ce71a5e92d0bf1
vchkoz <vchkoz@mail.ru>
2022-01-13 17:18:15 +0300

    Добавлена начальная страница

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 import csv
 import io
 
@@ -6,8 +6,12 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello, this is the WHITE BREAD page'
+def main():
+    return render_template("login_main.html")\
+
+@app.route('/user')
+def user_page():
+    return 'user page'
 
 
 @app.route('/upload/<serial_number>', methods=['POST'])
@@ -15,10 +19,10 @@ def profile(serial_number):
     file = io.StringIO(request.files['file'].stream.read().decode("UTF-8"), newline=None)
     reader = csv.reader(file, delimiter=';')
     for line in reader:
-        print(line) # тут будут выводиться принятые данные в виде списка
-    print(serial_number) # тут будет выводиться серийный номер АКЭС
+        print(line)  # тут будут выводиться принятые данные в виде списка
+    print(serial_number)  # тут будет выводиться серийный номер АКЭС
 
-    #     отправить файл.csv можно через коммандную сторку:
+    #     отправить файл.csv можно через коммандную строку:
     #     curl -F "file=@example.csv" http://localhost:5000/upload/<Здесь будет серийный номер>
 
     return 'success'
