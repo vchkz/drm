4ee642d4e67f1b8951891ff3bfefd7c60276e1c8
AnastasiayA26 <75423962+AnastasiayA26@users.noreply.github.com>
2022-01-14 21:52:22 +0300

    авторизация пользователя, обработка ошибок

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-from flask import Flask, request, render_template, redirect
+from flask import Flask, request, render_template, flash, url_for, session, redirect
 import csv
 import io
 import dataBase
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "fhmvsktf678"
 
 
 @app.route('/')  # Главная
 def main():
-    return render_template("login_main.html")
+    return render_template("contact.html")
 
 
-@app.route('/user')  # Страница пользователя
-def user_page():
-    return 'user page'
+@app.route('/contact', methods=["POST", "GET"])  # вход пользователя
+def contact():
+    if request.method == "POST":
+        if len(request.form["email"]) >= 4 and len(request.form["psw"]) > 2:
+            flash("Авторизация выполнена")
+        else:
+            flash("Ошибка")
+    return render_template("contact.html")
+
+
+@app.route('/user/<username>')  # Страница пользователя
+def user(username):
+    return f"Профиль пользователя: {username}"
 
 
 @app.route('/admin')  # Страница админа
@@ -21,6 +32,17 @@ def admin_page():
     user_list_admin_page = [((str(i[0])), i[2]) for i in dataBase.get_users() if not i[1]]
     # print(dataBase.get_serial_numbers_access(7))
     return render_template("admin_page.html", user_list_admin_page=user_list_admin_page)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    if 'userLogged' in session:
+        return redirect(url_for('user', username=session['userLogged']))
+    elif request.form['username'] == "test" and request.form['psw'] == "test":
+        session['userLogged'] = request.form['email']
+        return redirect(url_for('admin', username=session['userLogged']))
+
+    return render_template('login_main.html', title="Авторизация")
 
 
 @app.route('/admin/new-user', methods=['GET', 'POST'])  # Страница создания нового пользователя
@@ -49,6 +71,11 @@ def profile(serial_number):
     #     curl -F "file=@example.csv" http://localhost:5000/upload/<Здесь будет серийный номер>
 
     return 'success'
+
+
+@app.errorhandler(404)
+def pageNotFound(error):  # поиск не существующей страницы
+    return render_template("404.html"), 404
 
 
 if __name__ == '__main__':
