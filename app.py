60f922234d385e87296de8d44b9aff4ef780e0c1
vchkoz <vchkoz@mail.ru>
2022-01-15 20:43:07 +0300

    Добавлены функции редактирования пользователей

--- app.py
+++ app.py
@@ -27,10 +27,43 @@ def user(username):
     return f"Профиль пользователя: {username}"
 
 
-@app.route('/admin')  # Страница админа
+@app.route('/admin/delete-serial-number', methods=['POST'])  # Вспомогательная страница (Зайти сюда нельзя)
+def delete_serial_number():
+    if request.method == 'POST':
+        del_sernum = request.form['delete_sernum']
+        print(del_sernum)  # ещё не готово
+    return redirect('/admin')
+
+@app.route('/admin', methods=['GET', 'POST'])  # Страница админа
 def admin_page():
-    user_list_admin_page = [((str(i[0])), i[2]) for i in dataBase.get_users() if not i[1]]
-    # print(dataBase.get_serial_numbers_access(7))
+    if request.method == 'POST':
+        new_serial_number = request.form['ser_num']
+        to_user = request.form['user']
+        try:
+            dataBase.add_serial_number(new_serial_number)
+        except:
+            pass
+
+        try:
+            if dataBase.get_serial_number_id(new_serial_number) \
+                    not in dataBase.get_serial_numbers_access(dataBase.get_user_id(to_user)):
+                dataBase.add_access(to_user, new_serial_number)
+            else:
+                return '''
+                      <h1>Ошибка</h1>
+                      <h3>У данного пользователя уже есть доступ к этой АКЭС</h3>
+                      <a href="/admin">Вернуться назад</a>
+                      '''
+        except:
+            pass
+
+        return redirect('/admin')
+
+
+    user_list_admin_page = [(list(map(lambda x: dataBase.get_serial_number(x),
+                                      dataBase.get_serial_numbers_access(i[0]))), i[2])
+                            for i in dataBase.get_users() if not i[1]]
+
     return render_template("admin_page.html", user_list_admin_page=user_list_admin_page)
 
 
@@ -51,10 +84,25 @@ def create_user():
         new_login = request.form['new_login']
         new_password = request.form['psw']
         new_serial_number = request.form['serial_number']
-        dataBase.add_user(0, new_login, new_password)
-        dataBase.add_serial_number(new_serial_number)
+
+
+        if new_login not in list(map(lambda x: x[2], dataBase.get_users())):
+            dataBase.add_user(0, new_login, new_password)
+        else:
+            return '''
+                  <h1>Ошибка</h1>
+                  <h3>Пользователь с таким логином уже существует</h3>
+                  <a href="/admin/new-user">Вернуться назад</a>
+                  '''
+        try:
+            dataBase.add_serial_number(new_serial_number)
+        except:
+            pass
+
+
         dataBase.add_access(new_login, new_serial_number)
-        return redirect('/')
+
+        return redirect('/admin')
 
     return render_template("create_user.html")
 
